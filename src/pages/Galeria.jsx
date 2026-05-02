a18b128f0c0156c9556b134ff1cf5471841b0178
Adalia Flores Escobar <adaliaflores13@gmail.com>
2026-05-02 14:00:37 -0400

    feat: implement carousel navigation logic

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 function Galeria() {
+  const [index, setIndex] = useState(0);
+
+  const siguiente = () => {
+    setIndex(index + 1);
+  };
+
+  const anterior = () => {
+    setIndex(index - 1);
+  };
+
   return (
     <div className="galeria">
       <h1>Galería</h1>
 
       <div className="carrusel">
-        <button className="galeria-btn prev">❮</button>
+        <button className="galeria-btn prev" onClick={anterior}>
+          ❮
+        </button>
 
         <img
           src="/images/ajedrez1.jpg"
@@ -12,11 +26,12 @@ function Galeria() {
           className="galeria-img"
         />
 
-        <button className="galeria-btn next">❯</button>
+        <button className="galeria-btn next" onClick={siguiente}>
+          ❯
+        </button>
       </div>
     </div>
   );
 }
-
 
 export default Galeria;
