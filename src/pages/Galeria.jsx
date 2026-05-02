81c1cf5b6bcd88e07e77b830b66704b7f2b1d902
Adalia Flores Escobar <adaliaflores13@gmail.com>
2026-05-02 14:11:09 -0400

    feat: add image list for gallery carousel

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
 function Galeria() {
+  const imagenes = [
+  "/assets/ajedrez1.jpg",
+  "/assets/ajedrez2.jpg",
+  "/assets/ajedrez3.jpg",
+];
+
   const [index, setIndex] = useState(0);
 
   const siguiente = () => {
@@ -20,11 +26,7 @@ function Galeria() {
           ❮
         </button>
 
-        <img
-          src="/images/ajedrez1.jpg"
-          alt="Galería ajedrez"
-          className="galeria-img"
-        />
+        <img src={imagenes[index]} alt="Galería ajedrez" className="galeria-img" />
 
         <button className="galeria-btn next" onClick={siguiente}>
           ❯
